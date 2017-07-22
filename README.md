a9ff1ae87509de69a6e900f5c3084b08b28ff277
mpiern01 <marcpierne@gmail.com>
2017-07-22 21:22:09 +0000

    Ran console.log tests to transmit data to and from sockets

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# real_time_chat_socketio
-# Task 1 = Set up express app
-# Task 2 = Set up Socket.IO 
+# real_time_chat_socketio 
+Developed on Cloud9IDE and Local Atom using Circle CI Pushing to Heroku.
+*Note When looking at port listeners Cloud9 uses process.env.PORT, process.env.IP vs your own local port so I commented these out
+Task 1 = Set up express app
+Task 2 = Set up Socket.IO
+Task 3 = Testing moving data via console.log
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -22,13 +22,20 @@ io = sio.listen(server);
 // set socket.io debugging
 io.set('log level', 1);
 
-
-
-
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) =>{
     console.log('New user connected');
+    
+    socket.emit('newEmail', {
+        from: 'Mike@example.com',
+        text: 'Hey. Whats is going on?',
+        createAt: 123
+    });
+    
+socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+});    
     
 socket.on   ('disconnect', () => {
     console.log('User was disconnected');
